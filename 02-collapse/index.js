9c2a30248bcb1a1bc548b2892dcc7ce370fea80f
sviatlana <sviatlana.skiba@gmail.com>
2022-08-18 12:18:57 +0300

    fixes

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,24 +1,41 @@
-const content = document
-    .getElementsByClassName("collapsible__content")[0]
+const content = document.querySelector('.collapsible__content');
+console.log(content);
+if (content) {
+    content.style.overflow = 'hidden';
 
-content.style.overflow = 'hidden';
+    const anim = content
+        .animate({ height: ["0px", "auto"] }, { duration: 250, fill: "both", easing: 'ease-in-out' });
 
-const anim = content
-    .animate({ height: ["0px", "auto"] }, { duration: 250, fill: "both", easing: 'ease-in-out' });
+    anim.pause();
 
-anim.pause();
+    const button = document.querySelector('.collapsible__button');
+    const actionHidden = document.querySelector('.collapsible__action--hidden');
+    const actionVisible = document.querySelector('.collapsible__action--visible');
 
-document.getElementsByClassName("collapsible__button")[0].addEventListener("click", () => {
-    if (anim.playState === "paused") {
-        anim.play();
-    } else {
-        anim.reverse();
+    console.log(button);
+    console.log(actionHidden);
+    console.log(actionVisible);
+    if (button && actionHidden && actionVisible) {
+        actionHidden.style.visibility = 'hidden';
+        actionVisible.style.visibility = 'initial';
+        button.addEventListener("click", () => {
+            if (anim.playState === "paused") {
+                anim.play();
+                actionHidden.style.visibility = 'initial'
+                actionVisible.style.visibility = 'hidden';
+            } else {
+                anim.reverse();
+                actionHidden.style.visibility = 'hidden';
+                actionVisible.style.visibility = 'initial';
+
+            }
+        });
     }
-});
 
-anim.onfinish = (evt) => {
-    console.log(
-        "animation completed",
-        evt.currentTime === 0 ? "closed" : "opened"
-    );
-};
+    anim.onfinish = (evt) => {
+        console.log(
+            "animation completed",
+            evt.currentTime === 0 ? "closed" : "opened"
+        );
+    };
+}
